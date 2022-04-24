61f7f3ca987f5055a28c1b1a65de14badbdf8fe6
myuna <masato.yuna1@gmail.com>
2022-04-24 13:01:44 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         let result = 2 + 2;
+//         assert_eq!(result, 4);
+//     }
+// }
+
+mod generator;
+pub fn print_random_number() {
+    let n = generator::gen_ran();
+    println!("Random u8 {}", n);
+}
